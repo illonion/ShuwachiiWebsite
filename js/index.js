2c94586747d01daa72a7cc007e7c57f47e6b3e00
illonion <zhou.lawrence.yz@gmail.com>
2021-06-23 14:31:13 +1000

    Fix Navigation and Page Transition

--- js/index.js
+++ js/index.js
@@ -1,32 +1,25 @@
 $(window).on('load', () => {
-    /* Generate Sakura Falling */
     $('#bodies').sakura();
 
-    /* Icons */
     $('.icon')
-        .mouseover(() => {
-            var src = $(this).attr("src").match(/[^\.]+/) + "_hover.png";
-            $(this).attr("src", src);
-        })
-        .mouseout(() => {
-            var src = $(this).attr("src").replace("_hover.png", ".png");
-            $(this).attr("src", src);
-        });
-    
-    /* Navigation between pages */
-    var currentLocation = "main-body";
-    const commLink = "#toCommissions"
-    
-    $(commLink).on('click', () => {
-        pageTransition(commLink);
-    });
+        .mouseover(function() { $(this).attr("src", $(this).attr("src").match(/[^\.]+/) + "_hover.png"); })
+        .mouseout(function() { $(this).attr("src", $(this).attr("src").replace("_hover.png", ".png")); });
 
-        /* Function for Clicking Navigation */
+    /* Navigation between pages */
+    var currentLocation = "#main-body";
+    const homeLink = "#toMainBody";
+    const commLink = "#toCommissions";
+        
+    $(homeLink).on('click', function() { pageTransition("#main-body"); });
+    $(commLink).on('click', function() { pageTransition("#commissions"); });
+    
+    
+    /* Function for Clicking Navigation */
     function pageTransition(resultLocation) {
-        $(resultLocation).on('click', () => {
+        if (resultLocation != currentLocation) {
             $(currentLocation).fadeOut(1000);
             $(resultLocation).fadeIn(1000);
             currentLocation = resultLocation;
-        });
+        }
     }
 });
