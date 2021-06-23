f97c324a8a757463c1d491667eb9f0266e45cb59
illonion <zhou.lawrence.yz@gmail.com>
2021-06-23 12:01:22 +1000

    Add Commissions Tab, ChChange Navigation Script structure for mo scalability

--- js/index.js
+++ js/index.js
@@ -1,18 +1,32 @@
 $(window).on('load', () => {
+    /* Generate Sakura Falling */
     $('#bodies').sakura();
 
+    /* Icons */
     $('.icon')
-        .mouseover(function() {
+        .mouseover(() => {
             var src = $(this).attr("src").match(/[^\.]+/) + "_hover.png";
             $(this).attr("src", src);
         })
-        .mouseout(function() {
+        .mouseout(() => {
             var src = $(this).attr("src").replace("_hover.png", ".png");
             $(this).attr("src", src);
         });
     
-    $('#toCommissions').on('click', function() {
-        $('#main-body').fadeOut(1000);
-        $('#main-body').fadeIn(1000);
+    /* Navigation between pages */
+    var currentLocation = "main-body";
+    const commLink = "#toCommissions"
+    
+    $(commLink).on('click', () => {
+        pageTransition(commLink);
     });
+
+        /* Function for Clicking Navigation */
+    function pageTransition(resultLocation) {
+        $(resultLocation).on('click', () => {
+            $(currentLocation).fadeOut(1000);
+            $(resultLocation).fadeIn(1000);
+            currentLocation = resultLocation;
+        });
+    }
 });
