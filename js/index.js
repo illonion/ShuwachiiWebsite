e3026b2818272884a4214e5c4e3fe9c3c1e1c9ef
illonion <zhou.lawrence.yz@gmail.com>
2021-06-23 22:21:21 +1000

    Add Commission Page

--- js/index.js
+++ js/index.js
@@ -4,15 +4,23 @@ $(window).on('load', () => {
     $('.icon')
         .mouseover(function() { $(this).attr("src", $(this).attr("src").match(/[^\.]+/) + "_hover.png"); })
         .mouseout(function() { $(this).attr("src", $(this).attr("src").replace("_hover.png", ".png")); });
+    
+    /* Set all pages except the first to display nothing */
+    $("#commissions").hide();
 
     /* Navigation between pages */
-    var currentLocation = "#main-body";
+    var currentLocation = "#mainBody";
     const homeLink = "#toMainBody";
     const commLink = "#toCommissions";
+    const ruleLink = "#toRulesGuidelines";
+    const drawLink = "#toWillWontDraws";
+    const priceLink = "#toCommissionPrices";
         
-    $(homeLink).on('click', function() { pageTransition("#main-body"); });
+    $(homeLink).on('click', function() { pageTransition("#mainBody"); });
     $(commLink).on('click', function() { pageTransition("#commissions"); });
-    
+    $(ruleLink).on('click', function() { pageTransition("#rulesGuidelines"); });
+    $(drawLink).on('click', function() { pageTransition("#willWontDraws"); });
+    $(priceLink).on('click', function() { pageTransition("#commissionPrices"); });
     
     /* Function for Clicking Navigation */
     function pageTransition(resultLocation) {
